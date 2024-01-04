588e57e576a503356616381c30ec36e395205cd0
Tsingper <tsingper@outlook.com>
2024-01-04 19:47:45 +0800

    feat: page add

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   images: {
     domains: ['cdn.onc-prod.intergalactic.space'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatar.vercel.sh'
+      }
+    ]
   },
   // output: 'export',
 }
